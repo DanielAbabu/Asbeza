5bbebcc414d6fd1d3909a450bd1ad83f89afbd08
Abemchegen <abem.chegen@gmail.com>
2024-07-14 18:51:23 +0300

    added meal page

--- src/pages/meal/Meal.jsx
+++ src/pages/meal/Meal.jsx
@@ -1,7 +1,36 @@
-import "./meal.css"
+import "./meal.css";
 
-export default function Meal(){
-    return(
-        <div className="meal">Meal</div>
-    )
+export default function Meal() {
+  return (
+    <div className="meal-page">
+      <div className="meal-page-top">
+        <div className="trending-wrapper">
+          <div className="trending">
+            <div className="trending-details">
+              <h2> Trending Meal</h2>
+              <p>Smoked beef</p>
+              <p>meat, onion </p>
+            </div>
+            <img src="src\assets\steak.jpg" alt="beef" />
+          </div>
+        </div>
+
+        <div className="meal-ingrediants">
+          <div className="ingrediant-details">
+            <h2> Choose Ingrediants</h2>
+          </div>
+          <img src="src\assets\steak.jpg" alt="beef" />
+        </div>
+      </div>
+
+      <div className="meal-page-bottom">
+        <div className="meal-search">
+          <p>jfklajdljfkl</p>
+        </div>
+        <div className="meal-display">
+          <p>jfklajdljfkl</p>
+        </div>
+      </div>
+    </div>
+  );
 }
