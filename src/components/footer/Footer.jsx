4c1feae9fe7e98e3adc7519191240936a964d9c5
Abemchegen <abem.chegen@gmail.com>
2024-07-14 15:16:55 +0300

    footer added

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -1,7 +1,22 @@
-import "./footer.css"
+import "./footer.css";
 
-export default function Footer(){
-    return(
-        <div className="footer">Footer</div>
-    )
+export default function Footer() {
+  return (
+    <div className="footer-wrapper">
+      <div className="footer">
+        <div className="left-footer">
+          <h1>Asbeza</h1>
+          <p>
+            Our job is to bring you fresh and delicious ingrediants and with
+            fast delivery
+          </p>
+        </div>
+
+        <div className="right-footer">
+          <h3>Contact us</h3>
+          <input type="text" placeholder="write us your thoughts" />
+        </div>
+      </div>
+    </div>
+  );
 }
