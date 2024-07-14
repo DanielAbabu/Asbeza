0920a2c81c728a1bc2c0247b5207451f907ad6ae
DanielAbabu <Daniel0Ababu@gmail.com>
2024-07-14 14:16:58 +0300

    More assets

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -2,6 +2,19 @@ import "./navbar.css"
 
 export default function Navbar(){
     return(
-        <div className="navbar">Navbar</div>
+        <div className="navbar">
+            <div className="logo">ASBEZA</div>
+            <div className="page">
+                <a href="/home">Home</a>
+                <a href="/meal">Meal</a>
+                <a href="/ingredient">Ingredient</a>
+            </div>
+            <div className="right">
+
+                <img className="cart" src="" alt="" />
+                <div className="signin"><a href="/signin">Sign in</a></div>
+                <img className="profile" src="./src\assets\profile.jpg" alt="" />
+            </div>
+        </div>
     )
 }
