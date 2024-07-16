e58a164c2828ad7ea61dc144c445d58b78ccee2a
Abemchegen <abem.chegen@gmail.com>
2024-07-16 19:57:21 +0300

    added popup and dropdown

---
+++ src/components/popup/Popup.jsx
@@ -0,0 +1,138 @@
+import React, { useState, useEffect } from "react";
+import { useNavigate } from "react-router-dom";
+import Table from "react-bootstrap/Table";
+import "./popup.css";
+
+const Popup = ({ isVisible, togglePopup, meal, amount }) => {
+  const navigate = useNavigate();
+  const [rows, setRows] = useState([
+    {
+      id: 1,
+      item: "Tomatoes",
+      amount: 1 * amount + " unit",
+      price: 10 * amount,
+    },
+    {
+      id: 2,
+      item: "Lettuce",
+      amount: 1 * amount + " unit",
+      price: 20 * amount,
+    },
+    { id: 3, item: "Onion", amount: 3 * amount + " unit", price: 15 * amount },
+    { id: 4, item: "Meat", amount: 0.25 * amount + " kg", price: 250 * amount },
+    { id: 5, item: "Bread", amount: 1 * amount + " unit", price: 20 * amount },
+    { id: 6, item: "Flour", amount: 0.25 * amount + " kg", price: 25 * amount },
+  ]);
+
+  useEffect(() => {
+    if (rows.length === 0 && isVisible) {
+      togglePopup();
+    }
+  }, [rows, isVisible, togglePopup]);
+
+  useEffect(() => {
+    if (!isVisible) {
+      setRows([
+        {
+          id: 1,
+          item: "Tomatoes",
+          amount: 1 * amount + " unit",
+          price: 10 * amount,
+        },
+        {
+          id: 2,
+          item: "Lettuce",
+          amount: 1 * amount + " unit",
+          price: 20 * amount,
+        },
+        {
+          id: 3,
+          item: "Onion",
+          amount: 3 * amount + " unit",
+          price: 15 * amount,
+        },
+        {
+          id: 4,
+          item: "Meat",
+          amount: 0.25 * amount + " kg",
+          price: 250 * amount,
+        },
+        {
+          id: 5,
+          item: "Bread",
+          amount: 1 * amount + " unit",
+          price: 20 * amount,
+        },
+        {
+          id: 6,
+          item: "Flour",
+          amount: 0.25 * amount + " kg",
+          price: 25 * amount,
+        },
+      ]);
+    }
+  }, [isVisible, amount]);
+
+  const handleNavigate = () => {
+    navigate("/payment");
+  };
+
+  const handleDeleteRow = (id) => {
+    const newRows = rows.filter((row) => row.id !== id);
+    setRows(newRows);
+  };
+
+  const totalPrice = rows.reduce((total, row) => total + row.price, 0);
+
+  return (
+    isVisible && (
+      <div className="popup-overlay">
+        <div className="popup-content">
+          <div className="tabinfo">
+            <Table striped bordered hover className="custom-table">
+              <thead>
+                <tr>
+                  <th>Item</th>
+                  <th>Amount</th>
+                  <th>Price</th>
+                  <th></th>
+                </tr>
+              </thead>
+              <tbody>
+                {rows.map((row) => (
+                  <tr key={row.id}>
+                    <td>{row.item}</td>
+                    <td>{row.amount}</td>
+                    <td>{row.price} birr</td>
+                    <td>
+                      <button
+                        className="delete-btn"
+                        onClick={() => handleDeleteRow(row.id)}
+                      >
+                        X
+                      </button>
+                    </td>
+                  </tr>
+                ))}
+                <tr className="total-row">
+                  <td className="totalprice">Total Price</td>
+                  <td></td>
+                  <td className="font-weight-bold">{totalPrice} birr</td>
+                  <td></td>
+                </tr>
+              </tbody>
+            </Table>
+          </div>
+          <button className="close-btn" onClick={togglePopup}>
+            Cancel
+          </button>
+          <button className="checkout-btn" onClick={handleNavigate}>
+            Checkout
+          </button>
+        </div>
+      </div>
+    )
+  );
+};
+
+export default Popup;
--- src/pages/mealdetail/Mealdetail.jsx
+++ src/pages/mealdetail/Mealdetail.jsx
@@ -1,69 +1,86 @@
-import { useState } from 'react';
+import { useState } from "react";
 import "./mealdetail.css";
-import { CgAddR,CgRemoveR  } from "react-icons/cg";
-import Ingred from './../../components/Ingred/Ingred';
+import { CgAddR, CgRemoveR } from "react-icons/cg";
+import Ingred from "./../../components/Ingred/Ingred";
+import Popup from "../../components/popup/Popup";
 
+export default function Mealdetail() {
+  const [amount, setAmount] = useState(1);
+  const [popupVisible, setPopupVisible] = useState(false);
 
-export default  function Mealdetail(){
+  const increment = () => {
+    setAmount(amount + 1);
+  };
 
-    const [amount, setAmount] = useState(1);
+  const decrement = () => {
+    if (amount > 1) {
+      setAmount(amount - 1);
+    }
+  };
+  const togglePopup = () => {
+    setPopupVisible(!popupVisible);
+  };
 
-    const increment = () => {
-      setAmount(amount + 1);
-    };
-  
-    const decrement = () => {
-      if (amount > 1) {
-        setAmount(amount - 1);
-      }
-    };
+  return (
+    <div className="mealdetail">
+      <Popup
+        isVisible={popupVisible}
+        togglePopup={togglePopup}
+        meal={"cheese burger"}
+        amount={amount}
+      />
+      <div
+        className="pics"
+        style={{ backgroundImage: `url("./src/assets/food2.png")` }}
+      ></div>
+      <div className="detail">
+        <div className="txts">
+          <h3>Cheese Burger</h3>
+          <p>
+            with Bacon, Old Bay Fries, Caramelized Onion & Special Sauce with
+            Bacon, Old Bay Fries, Caramelized Onion & Special Sauce
+          </p>
 
-
-    return(
-        <div className="mealdetail">
-            <div className="pics" style={{ backgroundImage: `url("./src/assets/food2.png")` }}></div>
-            <div className="detail">
-                <div className="txts">
-                    <h3>Cheese Burger</h3>
-                    <p>with Bacon, Old Bay Fries, Caramelized Onion & Special Sauce with Bacon, Old Bay Fries, Caramelized Onion & Special Sauce</p>
-                    
-                    <div className="time">
-                    <h4>Total Preparation Time</h4> 
-                    <p>30 - 40 minutes</p>
-                    </div>
-                </div>
-
-                <div className="pricing">
-                    <div className="amt">
-                        <span>Serving amount</span>
-                        <div className="ctrl">
-                            <button onClick={decrement} className="control-btn">-</button>
-                            <input type="text" value={amount} readOnly />
-                            <button onClick={increment} className="control-btn">+</button>
-                        </div>
-                    </div>
-                    <div className="price">
-                        <span>Total Price</span>
-                        <span className='am' >{amount *340} Birr</span>
-                    </div>
-                    <div className="btn">
-                        Add
-                    </div>
-                </div>
-            </div>
-
-            <h2>Ingredient</h2>
-            <hr className='divider' />
-            <div className="inglis">
-                <Ingred/>
-                <Ingred/>
-                <Ingred/>
-                <Ingred/>
-                <Ingred/>
-                <Ingred/>
-                <Ingred/>
-
-            </div>
+          <div className="time">
+            <h4>Total Preparation Time</h4>
+            <p>30 - 40 minutes</p>
+          </div>
         </div>
-    )
+
+        <div className="pricing">
+          <div className="amt">
+            <span>Serving amount</span>
+            <div className="ctrl">
+              <button onClick={decrement} className="control-btn">
+                -
+              </button>
+              <input type="text" value={amount} readOnly />
+              <button onClick={increment} className="control-btn">
+                +
+              </button>
+            </div>
+          </div>
+          <div className="price">
+            <span>Total Price</span>
+            <span className="am">{amount * 340} Birr</span>
+          </div>
+          <div className="btn" onClick={togglePopup}>
+            Add
+          </div>
+        </div>
+      </div>
+
+      <h2>Ingredient</h2>
+      <hr className="divider" />
+      <div className="inglis">
+        <Ingred />
+        <Ingred />
+        <Ingred />
+        <Ingred />
+        <Ingred />
+        <Ingred />
+        <Ingred />
+      </div>
+    </div>
+  );
 }
