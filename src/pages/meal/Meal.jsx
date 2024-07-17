d898c946258d461a5c1ddc7555a0c9f7b4344cb7
DanielAbabu <Daniel0Ababu@gmail.com>
2024-07-17 12:52:22 +0300

    bugs

--- src/pages/meal/Meal.jsx
+++ src/pages/meal/Meal.jsx
@@ -3,6 +3,9 @@ import Mealcard from "../../components/mealcard/Mealcard";
 import Trending from "../../components/trending/Trending";
 import Choose from "../../components/choose/Choose";
 import Search from "../../components/search/Search";
+import food1 from '../../assets/food1.png';
+import food3 from '../../assets/food3.png';
+
 export default function Meal() {
   const mealCards = Array.from({ length: 20 });
   return (
@@ -12,14 +15,14 @@ export default function Meal() {
           <Trending
             name="Smoked Beef"
             ing="Meat, Chicken, Onion, Spices"
-            src={"src\\assets\\food1.png"}
+            src={food1}
             alt={"smoked beef"}
           />
         </div>
         <Choose
           title="Choose Ingredient"
           link="/ingredient"
-          src={"src\\assets\\food3.png"}
+          src={food3}
         />
       </div>
 
